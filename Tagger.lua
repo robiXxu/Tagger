8e0e28e49ee8582bdee162e6065c1095348a5d45
Schiriac Robert <schiriac.robert@gmail.com>
2019-08-31 15:00:57 +0300

    update

--- Tagger.lua
+++ Tagger.lua
@@ -2,6 +2,8 @@ local TAGGER_FRAME = CreateFrame("FRAME", "Tagger")
 TAGGER_FRAME:RegisterEvent("UNIT_TARGET")
 local function eventHandler(self, event, ...)
   if #{...} > 0 then
+    local name = UnitName("target")
+    print("Selected " .. name)
     SetRaidTarget("target", 8)
   end
 end
